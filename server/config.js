f22666884551a6431d3514ec0b26a6e5032eac8d
OJDylan <ojdylan.my@gmail.com>
2022-03-24 20:20:02 +0800

    insert config.js

---
+++ server/config.js
@@ -0,0 +1,12 @@
+const config = {
+  db: {
+    host: "localhost",
+    user: "root",
+    password: "insert pw here",
+    port: 3306,
+    database: "test_db",
+  },
+  listPerPage: 10,
+};
+
+module.exports = config;
